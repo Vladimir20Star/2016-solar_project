3977f26a8535c9cc0914ea048738022854e5708c
Vladimir20Star <starchenko.vv@phystech.edu>
2021-11-10 13:20:42 +0300

    убрал ошибку utf-8

--- solar_input.py
+++ solar_input.py
@@ -5,11 +5,10 @@ from solar_objects import Star, Planet
 
 
 def read_space_objects_data_from_file(input_filename):
-    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
+    """Считывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
 
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -19,7 +18,7 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line.split(), star)
                 objects.append(star)
@@ -96,8 +95,6 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(f"{type(obj)} {obj.R} {obj.color} {obj.m} {obj.x} {obj.y} {obj.Vx} {obj.Vy}", file=out_file)
-
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 
 if __name__ == "__main__":
